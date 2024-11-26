330d3a8b74c53f671de357dda080419ab8c7152f
rebecaozao <132940584+rebecaozao@users.noreply.github.com>
2024-11-26 09:39:08 -0300

    Add files via upload

---
+++ Flashcard 2b/perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral descobriu o Brasil'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem é o atual lider da temporada da Fórmula 1',
+    'O lider é Max Verstappen'
+)
